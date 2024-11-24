47e3d569fc578838ff7d2fcf2ce9afedf5fdaf56
akilesh-io <2112akilesh@gmail.com>
2024-11-25 00:00:20 +0530

    fix: :zap: perfected structured and added some contents in it

--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,18 @@
-'use client';
-import Image from "next/image"
-import saLogo from "./logo.svg";
-
-import { useEffect, useState } from "react";
-import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
-import "../sass/main.scss";
-
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import Loader from "../components/Loader";
 import Hero from "@/sections/hero";
-
-// export default function Home() {
-//   return (
-//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-//         <Image
-//           className="dark:invert"
-//           src="https://swami-associates.in/logo.jpg"
-//           alt="Mettupalayam"
-//           width={180}
-//           height={38}
-//           priority
-//         />    
-//         <div className="flex gap-4 items-center flex-col sm:flex-row">
-//           <a
-//             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <Image              
-//               src={saLogo}
-//               alt="construction"
-//               width={20}
-//               height={20}
-//             />
-//             Swami
-//           </a>
-//           <a
-//             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-//             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Associates
-//           </a>
-//         </div>
-//       </main>
-//     </div>
-//   );
-// }
+import About from "@/sections/about";
+import Team from "@/sections/team";
+import Footer from "@/sections/footer";
 
 export default function Home() {
 
   return (
     <>
       <Hero />
+      <About />
+      {/* <TimeTested /> */}
+      {/* <Works /> */}
+      <Team />
+      <Footer />
     </>
   );
 }
--- sections/footer.tsx
+++ sections/footer.tsx
@@ -1,9 +1,27 @@
-export default function Address() {
+export default function Footer() {
 
     return (
-        <div>
-            <h1>2B CRK BUILDING,ANNAJI RAO ROAD, METTUPALAYAM, COIMBATORE.641104    </h1>
-        </div>
+        <footer>
+            <h1>
+                Social Media
+            </h1>
+            <h3>
+                <a href="https://www.instagram.com/swamiassociates_mtp?igsh=MWhwaGtucXJjYXpuOA==">Instagram</a>
+            </h3>
+
+            <h1>Address:</h1>
+            <h3>
+                2B CRK BUILDING,ANNAJI RAO ROAD, METTUPALAYAM, COIMBATORE.641104
+            </h3>
+            <h1>Contact us</h1>
+            <h3>
+                <a href="mailto:swamiassociatesmtp@gmail.com">Email</a>
+                <a href="tel:+919865619755">Office Number</a>
+                <a href="tel:+919842219755">Mobile Number</a>
+            </h3>
+
+        </footer>
     )
 }
+
 
