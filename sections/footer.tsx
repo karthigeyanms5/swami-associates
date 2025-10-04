a53fb4588e77067513bbb3224f9cae96c79be6e2
akilesh-io <2112akilesh@gmail.com>
2025-10-04 11:19:58 +0530

    social media link updated in footer

--- sections/footer.tsx
+++ sections/footer.tsx
@@ -77,7 +77,7 @@ const Footer = () => {
               transition={{ duration: 1, delay: 0.4 }}
               className="flex flex-wrap gap-8 w-full justify-center md:justify-start"            >
               <a
-                href="https://www.instagram.com/swamiassociates_mtp?igsh=MWhwaGtucXJjYXpuOA=="
+                href="https://www.instagram.com/swamiassociates_mtp"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:underline"
@@ -85,7 +85,7 @@ const Footer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" className="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
               </a>
               <a
-                href="https://www.linkedin.com"
+                href="https://www.linkedin.com/in/swaminathan-a-614010a9/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:underline"
