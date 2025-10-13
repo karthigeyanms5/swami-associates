96b2368fe4053365b27293291dd8a8d203550e2d
akilesh-io <2112akilesh@gmail.com>
2025-10-14 00:41:07 +0530

    contact us  font change to georgia

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -12,8 +12,8 @@ export default function ContactUs() {
           {/* Left text section */}
           <div className="max-w-md text-left">
             <h2
-              className="text-3xl sm:text-4xl md:text-5xl text-gray-900 leading-snug"
-              style={{ fontFamily: "var(--font-minion)" }}
+              className="text-3xl sm:text-4xl md:text-5xl text-gray-900 leading-snug font-serif"
+              // style={{ fontFamily: "var(--font-minion)" }}
             >
               Ready to build <br /> your dream home? <br />
               Get in touch with us.
@@ -117,7 +117,44 @@ export function Form() {
         </div>
 
         {/* Submit button aligned right */}
-        <div className="flex justify-end">
+        <div className="flex justify-between">
+          <h2 className="flex">
+            <svg
+              className="w-5 me-2"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-phone"
+            >
+              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
+            </svg>
+            9842219755{" "}
+          </h2>
+          <h2 className="flex">
+            <svg
+              className="w-5 me-2"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-mail"
+            >
+              <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
+              <polyline points="22,6 12,13 2,6" />
+            </svg>
+            swamiassociatesmtp@gmail.com
+          </h2>
           <button
             type="submit"
             disabled={loading}
