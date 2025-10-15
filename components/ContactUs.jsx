b076dc697a22c63b4085782d0e1de64856754324
akilesh-io <2112akilesh@gmail.com>
2025-10-16 00:14:44 +0530

    added phont and mail link

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -119,7 +119,10 @@ export function Form() {
         {/* Submit button aligned right */}
         <div className="flex flex-col sm:flex-row sm:justify-between gap-4 sm:items-center">
           {/* Phone */}
-          <h2 className="flex items-center text-sm sm:text-base">
+          <a
+            href="tel:9842219755"
+            className="flex items-center text-sm sm:text-base text-black hover:underline"
+          >
             <svg
               className="w-5 h-5 me-2"
               xmlns="http://www.w3.org/2000/svg"
@@ -133,10 +136,13 @@ export function Form() {
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
             </svg>
             9842219755
-          </h2>
+          </a>
 
           {/* Email */}
-          <h2 className="flex items-center text-sm sm:text-base">
+          <a
+            href="mailto:swamiassociatesmtp@gmail.com"
+            className="flex items-center text-sm sm:text-base text-black hover:underline"
+          >
             <svg
               className="w-5 h-5 me-2"
               xmlns="http://www.w3.org/2000/svg"
@@ -151,7 +157,7 @@ export function Form() {
               <polyline points="22,6 12,13 2,6" />
             </svg>
             swamiassociatesmtp@gmail.com
-          </h2>
+          </a>
 
           {/* Button */}
           <button
