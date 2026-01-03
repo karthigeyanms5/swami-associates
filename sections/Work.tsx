d4f6ef609957527eb33aa5daa18e5e5218bfe014
karthigeyanms5 <karthigeyanms5@gmail.com>
2026-01-03 16:11:41 +0530

    Update Work.tsx

--- sections/Work.tsx
+++ sections/Work.tsx
@@ -21,7 +21,6 @@ const projects: Project[] = [
   { title: "Mr.RSR Residence", location: "Mettupalayam", image: "/slider/rsr_srinvasan.png" },
   { title: "Mr.Sampath Residence", location: "Mettupalayam", image: "/slider/krg_sampath.png" },
   { title: "Sakthi Hospital", location: "Mettupalayam", image: "/slider/sakthi_hospital.png" },
-  { title: "Eye Foundation Complex", location: "Mettupalayam", image: "/slider/eye_foundation.png" },
   { title: "GV Residence", location: "Mettupalayam", image: "/slider/giri_brindha_residence.png" },
   // { title: "Saravanampatti", location: "Coimbatore", image: "https://images.unsplash.com/photo-1523217582562-09d0def993a6?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&fm=jpg&q=60&w=3000" },
   // { title: "Downtown Complex", location: "Chennai", image: "https://images.unsplash.com/photo-1709147617968-709368900af1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764" },
