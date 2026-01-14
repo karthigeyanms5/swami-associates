78fe03de71dee9d93b4493a3312aa9fc44c719e0
karthigeyanms5 <karthigeyanms5@gmail.com>
2026-01-15 00:10:28 +0530

    Update team.tsx

--- sections/team.tsx
+++ sections/team.tsx
@@ -5,7 +5,7 @@ const teamMembers = [
         id: 1,
         name: "Er.Swaminathan.A",
         description: "Managing Director",
-        image: "team/Mr.Swaminathan_Photo.jpeg",
+        image: "team/Mr.Swaminathan_Photo_0003.png",
     },
     {
         id: 2,
